ff8cd7440e08f8ffdab04a59e64ffcdad64d7963
Zuiron <martinfloden90@gmail.com>
2021-10-03 01:46:50 +0200

    last try still not working.

--- src/main/java/net/zuiron/huntergatherers/client/HunterGatherersClient.java
+++ src/main/java/net/zuiron/huntergatherers/client/HunterGatherersClient.java
@@ -1,8 +1,10 @@
 package net.zuiron.huntergatherers.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.impl.blockrenderlayer.BlockRenderLayerMapImpl;
 import net.minecraft.client.render.RenderLayer;
+import net.zuiron.huntergatherers.HunterGatherers;
 import net.zuiron.huntergatherers.block.ModBlocks;
 
 @SuppressWarnings("unused")
@@ -11,6 +13,8 @@ public class HunterGatherersClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         //BlockRenderLayerMapImpl.INSTANCE.putBlocks(RenderLayer.getCutout(), ModBlocks.FLORAMELISSIA);
-        BlockRenderLayerMapImpl.INSTANCE.putBlocks(RenderLayer.getTranslucent(), ModBlocks.FLORAMELISSIA);
+        //BlockRenderLayerMapImpl.INSTANCE.putBlocks(RenderLayer.getTranslucent(), ModBlocks.FLORAMELISSIA);
+        //BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FLORAMELISSIA, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FLORAMELISSIA, RenderLayer.getTranslucent());
     }
 }
