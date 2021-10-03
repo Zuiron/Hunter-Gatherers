26d688ec9066c1d061b7380739554136202de51c
Zuiron <martinfloden90@gmail.com>
2021-10-03 13:55:46 +0200

    flint knife now gives bark when stripping logs.

--- src/main/java/net/zuiron/huntergatherers/item/ModItems.java
+++ src/main/java/net/zuiron/huntergatherers/item/ModItems.java
@@ -1,15 +1,29 @@
 package net.zuiron.huntergatherers.item;
 
+import com.google.common.collect.BiMap;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.*;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import net.zuiron.huntergatherers.HunterGatherers;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
 
 class PotatoToolMaterial implements ToolMaterial {
 
@@ -17,22 +31,22 @@ class PotatoToolMaterial implements ToolMaterial {
 
     @Override
     public int getDurability() {
-        return 0;
+        return 59;
     }
 
     @Override
     public float getMiningSpeedMultiplier() {
-        return 0;
+        return 2.0F;
     }
 
     @Override
     public float getAttackDamage() {
-        return 0;
+        return 0.0F;
     }
 
     @Override
     public int getMiningLevel() {
-        return 0;
+        return -1;
     }
 
     @Override
@@ -46,15 +60,86 @@ class PotatoToolMaterial implements ToolMaterial {
     }
 }
 
-class CustomPickaxeItem extends PickaxeItem {
-    public CustomPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
-        super(material, attackDamage, attackSpeed, settings);
-    }
-}
-
 class CustomAxeItem extends AxeItem {
+
     public CustomAxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
+    }
+
+    public int getRandomNumberUsingNextInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        PlayerEntity playerEntity = context.getPlayer();
+        BlockState blockState = world.getBlockState(blockPos);
+        Optional<BlockState> optional = this.getStrippedState(blockState);
+        Optional<BlockState> optional2 = Oxidizable.getDecreasedOxidationState(blockState);
+        Optional<BlockState> optional3 = Optional.ofNullable((Block)((BiMap)HoneycombItem.WAXED_TO_UNWAXED_BLOCKS.get()).get(blockState.getBlock())).map((block) -> {
+            return block.getStateWithProperties(blockState);
+        });
+
+        ItemStack itemStack = context.getStack();
+        Optional<BlockState> optional4 = Optional.empty();
+        if (optional.isPresent()) {
+            world.playSound(playerEntity, blockPos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            optional4 = optional;
+
+            //used on a log.
+
+            //get block which we just stripped
+            Block blockStripped = context.getWorld().getBlockState(blockPos).getBlock();
+
+            if(blockStripped == Blocks.OAK_LOG) {
+                Block.dropStack(world,blockPos,new ItemStack(ModItems.BARK_OAK,getRandomNumberUsingNextInt(0,2)));
+            } else if(blockStripped == Blocks.BIRCH_LOG) {
+                Block.dropStack(world,blockPos,new ItemStack(ModItems.BARK_BIRCH,getRandomNumberUsingNextInt(0,2)));
+            } else if(blockStripped == Blocks.ACACIA_LOG) {
+                Block.dropStack(world,blockPos,new ItemStack(ModItems.BARK_ACACIA,getRandomNumberUsingNextInt(0,2)));
+            } else if(blockStripped == Blocks.DARK_OAK_LOG) {
+                Block.dropStack(world,blockPos,new ItemStack(ModItems.BARK_DARK_OAK,getRandomNumberUsingNextInt(0,2)));
+            } else if(blockStripped == Blocks.SPRUCE_LOG) {
+                Block.dropStack(world,blockPos,new ItemStack(ModItems.BARK_SPRUCE,getRandomNumberUsingNextInt(0,2)));
+            } else if(blockStripped == Blocks.JUNGLE_LOG) {
+                Block.dropStack(world,blockPos,new ItemStack(ModItems.BARK_JUNGLE,getRandomNumberUsingNextInt(0,2)));
+            }
+
+        } else if (optional2.isPresent()) {
+            world.playSound(playerEntity, blockPos, SoundEvents.ITEM_AXE_SCRAPE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            world.syncWorldEvent(playerEntity, 3005, blockPos, 0);
+            optional4 = optional2;
+        } else if (optional3.isPresent()) {
+            world.playSound(playerEntity, blockPos, SoundEvents.ITEM_AXE_WAX_OFF, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            world.syncWorldEvent(playerEntity, 3004, blockPos, 0);
+            optional4 = optional3;
+        }
+
+        if (optional4.isPresent()) {
+            if (playerEntity instanceof ServerPlayerEntity) {
+                Criteria.ITEM_USED_ON_BLOCK.trigger((ServerPlayerEntity)playerEntity, blockPos, itemStack);
+            }
+
+            world.setBlockState(blockPos, (BlockState)optional4.get(), 11);
+            if (playerEntity != null) {
+                itemStack.damage(1, playerEntity, (p) -> {
+                    p.sendToolBreakStatus(context.getHand());
+                });
+            }
+
+            return ActionResult.success(world.isClient);
+        } else {
+            return ActionResult.PASS;
+        }
+    }
+
+    private Optional<BlockState> getStrippedState(BlockState state) {
+        return Optional.ofNullable((Block)STRIPPED_BLOCKS.get(state.getBlock())).map((block) -> {
+            return (BlockState)block.getDefaultState().with(PillarBlock.AXIS, (Direction.Axis)state.get(PillarBlock.AXIS));
+        });
     }
 }
 
