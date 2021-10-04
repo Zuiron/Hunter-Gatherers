455a9140cc6cf0aa3ebd8b7b242b78075e2c2ba3
Zuiron <martinfloden90@gmail.com>
2021-10-05 00:46:45 +0200

    loot table leaves

--- src/main/java/net/zuiron/huntergatherers/modifyLootTables.java
+++ src/main/java/net/zuiron/huntergatherers/modifyLootTables.java
@@ -18,6 +18,13 @@ public class modifyLootTables {
 
     private static final Identifier OAK_LEAVES_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/oak_leaves");
     private static final Identifier BIRCH_LEAVES_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/birch_leaves");
+    private static final Identifier DARK_OAK_LEAVES_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/dark_oak_leaves");
+    private static final Identifier JUNGLE_LEAVES_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/jungle_leaves");
+    private static final Identifier ACACIA_LEAVES_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/acacia_leaves");
+    private static final Identifier SPRUCE_LEAVES_BLOCK_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/spruce_leaves");
+
+
+
     //private static final Identifier WHITE_SHEEP_LOOT_TABLE_ID = new Identifier("minecraft", "entities/sheep/white");
     private static final Identifier COMMON_SHEEP_LOOT_TABLE_ID = new Identifier("minecraft", "entities/sheep");
     private static final Identifier GRASS_ID = new Identifier("minecraft", "blocks/grass");
@@ -44,11 +51,11 @@ public class modifyLootTables {
                         .rolls(ConstantLootNumberProvider.create(1))
                         .with(ItemEntry.builder(ModItems.GRASS_FIBRE_PIECE))
                         .withCondition(MatchToolLootCondition.builder(ItemPredicate.Builder.create().items(ModItems.FLINT_KNIFE)).build())
-                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 3.0f)).build());
                 supplier.withPool(poolBuilder.build());
             }
 
-            //modify oak leaves.
+            //modify leaves.
             if(OAK_LEAVES_BLOCK_LOOT_TABLE_ID.equals(id)) {
                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
@@ -58,19 +65,55 @@ public class modifyLootTables {
                         .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
                 supplier.withPool(poolBuilder.build());
             }
-
-            //modify birch leaves
+            if(DARK_OAK_LEAVES_BLOCK_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(ModItems.BRANCH_DARK_OAK))
+                        .with(ItemEntry.builder(ModItems.CONE_DARK_OAK))
+                        .with(ItemEntry.builder(ModItems.LEAF_DARK_OAK))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
             if(BIRCH_LEAVES_BLOCK_LOOT_TABLE_ID.equals(id)) {
                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .with(ItemEntry.builder(Items.STICK))
+                        .with(ItemEntry.builder(ModItems.BRANCH_BIRCH))
                         .with(ItemEntry.builder(ModItems.CONE_BIRCH))
                         .with(ItemEntry.builder(ModItems.LEAF_BIRCH))
                         .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
                 supplier.withPool(poolBuilder.build());
             }
+            if(ACACIA_LEAVES_BLOCK_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(ModItems.BRANCH_ACACIA))
+                        .with(ItemEntry.builder(ModItems.CONE_ACACIA))
+                        .with(ItemEntry.builder(ModItems.LEAF_ACACIA))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
+            if(JUNGLE_LEAVES_BLOCK_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(ModItems.BRANCH_JUNGLE))
+                        .with(ItemEntry.builder(ModItems.CONE_JUNGLE))
+                        .with(ItemEntry.builder(ModItems.LEAF_JUNGLE))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
+            if(SPRUCE_LEAVES_BLOCK_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(ModItems.BRANCH_SPRUCE))
+                        .with(ItemEntry.builder(ModItems.CONE_SPRUCE))
+                        .with(ItemEntry.builder(ModItems.LEAF_SPRUCE))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
 
-            //add sheep bone to "all/common" sheep.
+
+
+            //add sheep bone to "all/common" sheep. require special arrow? YES TODO
 			if(COMMON_SHEEP_LOOT_TABLE_ID.equals(id)) {
 				FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
 						.rolls(ConstantLootNumberProvider.create(1))
